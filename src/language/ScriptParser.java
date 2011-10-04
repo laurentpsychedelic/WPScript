e08a93466a440464421cebdf4132aa84f9a5dab9
Laurent FABRE <laurent_psychedelic@hotmail.com>
2011-10-04 09:07:19 +0000

    ①string literalでドットが扱えるようにした。
    git-svn-id: file:///home/laurent/dev/local_svn_repositories/WPAScript@29 b1083ead-136b-6749-9d02-a4e12f4b591d

--- src/language/ScriptParser.java
+++ src/language/ScriptParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 D:\\PA-WPA-View\\GUI\\SCRIPTING\\WPAScript\\working_copy\\src\\language\\Script.g 2011-10-04 17:28:06
+// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 D:\\PA-WPA-View\\GUI\\SCRIPTING\\WPAScript\\working_copy\\src\\language\\Script.g 2011-10-04 18:06:01
 
 package language;
 

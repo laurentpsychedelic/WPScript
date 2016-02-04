958b3061acd1bf255386f1eedbf0ecc50ad5e4f5
Laurent FABRE <laurent_psychedelic@hotmail.com>
2016-02-04 15:40:20 +0900

    RM #880 追加修正（改定されたファイルの冒頭の日付をも修正）

--- src/org/lpsy/wpscript/commandline/Main.java
+++ src/org/lpsy/wpscript/commandline/Main.java
@@ -8,7 +8,7 @@ import org.lpsy.wpscript.editor.ScriptWindow;
 import org.lpsy.wpscript.utilities.FileIO;
 
 /**
- * @author Laurent FABRE, 2011-2015
+ * @author Laurent FABRE, 2011-2016
  */
 public class Main {
     public static final String VERSION = "1.0";
